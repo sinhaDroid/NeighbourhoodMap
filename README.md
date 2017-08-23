61bc65fce26fe379f9178cde1aa546a181450f9a
sinhadroid <107sinha@gmail.com>
2017-08-23 05:58:16 +0530

    before removing comment

---
+++ README.md
@@ -0,0 +1,20 @@
+# Neighbourhood Map
+Find store, cafe, food and bar in different cities and countries and see which one you have not visited yet or plan your future visits. You can click on the markers and see the useful info about every places.
+
+In my project I have chosen Google Map Api. It's one page app that uses knockout.js binding.
+
+You can see the demo here: [https://neighbourhood-map.herokuapp.com/](https://neighbourhood-map.herokuapp.com/)
+
+# Tasks
+
+1. Download the project
+2. Open index.html file
+
+# Resources
+To build this project I used several sources:
+
+1. http://knockoutjs.com/
+2. BikeinCity project, http://www.codeproject.com/Articles/387626/BikeInCity-2-KnockoutJS-JQuery-Google-Maps
+3. http://knockoutjs.com/documentation/introduction.html
+4. https://www.packtpub.com/books/content/using-google-maps-apis-knockoutjs
+5. Various examples on internet.
--- js/script.js
+++ js/script.js
@@ -1,165 +1,169 @@
+/* Module for maps application */
+var MapsApplication = function () {
+  /* add members here */
+  var localLocation = {lat: 13.0622714, lng: 77.59625439999999};
+  var listofplaces = [];
 
-var map;
+  /* model to hold addresses */
+  var mapsModel = {
+    address: ko.observable(),
+    places: ko.observableArray(listofplaces),
 
-var markers = [];
-
-
-/* generic model for address */
-
-var AddressModel = function() {
-
-	this.marker = ko.observable();
-
-	this.location = ko.observable();
-
-	this.streetNumber = ko.observable();
-
-	this.streetName = ko.observable();
-
-	this.city = ko.observable();
-
-	this.state = ko.observable();
-
-	this.postCode = ko.observable();
-
-	this.country = ko.observable();
-
-};
-
-
-/* method to update address */
-
-var populateAddress = function(place) {
-
-  console.log(place);
-
-  // value().streetNumber(place);
-}
-
-
-/* method to place a marker on the map */
-
-var placeMarker = function (location, value) {
-
-	// create and place marker on the map
-
-	var marker = new google.maps.Marker({
-
-		position: location,
-
-		map: map
-
-	});
-
-	//store the newly created marker in the address model
-
-	value().marker(marker);
-
-};
-
-
-/* method to remove old marker from the map */
-
-var removeMarker = function(address) {
-
-	if(address != null) {
-
-		address.marker().setMap(null);
-
-	}
-
-};
-
-
-/* method to register subscriber */
-
-var registerSubscribers = function () {
-
-	//fire before from address is changed
-
-	mapsModel.forEach(function(mapModel){
-
-		mapModel.address.subscribe(function(oldValue){
-
-			removeMarker(oldValue);
-
-		}, null, "addressChange");
-	});
-
-};
-
-
-/* method to update the address model */
-
-var updateAddress = function(place, value) {
-
-	populateAddress(place);
-
-	placeMarker(place.geometry.location, value);
-
-};
-
-
-/* Initialize the map */
-
-function initialize() {
-
-	var mapOptions = {
-		center: {lat: -33.8688, lng: 151.2195},
-		zoom: 13,
-		scrollwheel: false
+    goToMarker: function(place) {
+      var name = place.sName();
+      for(var key in mapsModel.places()) {
+        if (mapsModel.places()[key].sName() === name) {
+          google.maps.event.trigger(
+            mapsModel.places()[key].marker(), 'click', function() {
+              infowindow.setContent(name);
+              infowindow.open(map, this);
+            }
+          );
+        }
+      }
+    }
   };
 
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  /* generic model for address */
+  var AddressModel = function() {
+    this.marker = ko.observable();
+    this.location = ko.observable();
+    this.sName = ko.observable();
+    this.sAddress = ko.observable();
+  };
 
-  var input = (document.getElementById('txtplaces'));
+  /* method to center map based on the location*/
+  var centerMap = function (location) {
+    map.setCenter(location);
+    map.setZoom(17);
+    google.maps.event.trigger(map, 'resize');
+  }
 
-  // Create the autocomplete helper, and associate it with
-  // an HTML text input box.
-  var autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.bindTo('bounds', map);
+  /* method to place a marker on the map */
+  var placeMarker = function (place, value) {
+    // create and place marker on the map
+    var pos = new google.maps.LatLng(
+      place.geometry.location.lat(),
+      place.geometry.location.lng()
+    );
+    marker = new google.maps.Marker({
+      position: pos,
+      map: map,
+      animation: google.maps.Animation.BOUNCE
+    });
+    //store the newly created marker in the address model
+    value().marker(marker);
 
-  var infowindow = new google.maps.InfoWindow();
+    setInfoContent(place.name);
+  };
 
-  var marker = new google.maps.Marker({
-    map: map
-  });
+  function setInfoContent(name) {
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(name);
+      infowindow.open(map, this);
+    });
+  }
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map, marker);
-  });
+  /* method to retrieve address information in the model */
+  var populateAddress = function (place, value) {
+    
+    var address = new AddressModel();
+    //set location
+    address.location(place.geometry.location);
+    address.sName(place.name);
+    address.sAddress(place.vicinity);
 
-  // Get the full place details when the user selects a place from the
-  // list of suggestions.
-  google.maps.event.addListener(autocomplete, 'place_changed', function() {
-    infowindow.close();
-    var place = autocomplete.getPlace();
-    if (!place.geometry) {
-      return;
-    }
+    //set the address model in the binding value
+    value(address);
 
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-    }
+    listofplaces.push(address);
+  };
 
-    updateAddress(place, marker);
+  /* method to update the address model */
+  var updateAddress = function(place, value) {
+    populateAddress(place, value);
+    placeMarker(place, value);
+  };
 
-    // Set the position of the marker using the place ID and location.
-    marker.setPlace(/** @type {!google.maps.Place} */ ({
-      placeId: place.place_id,
-      location: place.geometry.location
-    }));
-    marker.setVisible(true);
+  /* method to retrieve nearBy places */
+  function nearBySearch(location, value) {
+    var request = {
+      location: location,
+      radius: '500',
+      types: ['store', 'café','food', 'bar']
+    };
+    centerMap(location);
+    // Request for places library for nearby places of interest
+    new google.maps.places.PlacesService(map).nearbySearch(
+      request, function callback(results, status){
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            value(place);
+            updateAddress(place, value);
+          }
+        }
+      }
+    );
+  }
 
-    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-        'Place ID: ' + place.place_id + '<br>' +
-        place.formatted_address + '</div>');
-    infowindow.open(map, marker);
-  });
-}
+  /* method to retrieve and set local location */
+  var setLocalLocation = function (value) {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        localLocation.lat = position.coords.latitude;
+        localLocation.lng = position.coords.longitude;
+        nearBySearch(localLocation, value);
+        console.log("successfully retrieved local location. Lat [" + localLocation.lat + "] Lng [" + localLocation.lng + "]");
+      },
+      function (error) {
+        console.log("Could not get current coords: " + error.message);
+      });
+    };
+  };
 
-// Run the initialize function when the window has finished loading.
-google.maps.event.addDomListener(window, 'load', initialize);
+  /* method to add custom binding handlers to knockout */
+  var configureBindingHandlers = function() {
+    /* custom binding for address auto complete */
+    ko.bindingHandlers.addressAutoComplete = {
+      init: function(element, valueAccessor){
+        // create the autocomplete object
+        var autocomplete = new google.maps.places.Autocomplete( element,{ types: ['geocode'] });
+        // when the user selects an address from the dropdown, populate the address in the model.
+        var value = valueAccessor();
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+          var place = autocomplete.getPlace();
+          nearBySearch(place.geometry.location, value);
+        });
+      }
+    };
+
+    /* custom binding handler for maps panel   */
+    ko.bindingHandlers.mapPanel = {
+        init: function(element, valueAccessor){
+          map = new google.maps.Map(element, {
+            zoom: 13
+          });
+          infowindow = new google.maps.InfoWindow();
+          var value = valueAccessor();
+          centerMap(localLocation);
+          setLocalLocation(value);
+        }
+    };
+  };
+
+
+  var init = function () {
+    configureBindingHandlers()
+    /* add code to initialize this module */
+    ko.applyBindings(MapsApplication);
+  };
+
+  /* execute the init function when the DOM is ready */
+  $(init);
+  
+  return {
+    /* add members that will be exposed publicly */
+    mapsModel
+  };
+}();
